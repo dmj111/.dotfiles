27c1cd330a6b5050272493482ff9b8c82c68b1a5
Dave Jones <dmj111@gmail.com>
2016-11-06 09:13:10 -0500

    Add info to git prompt

--- bash/65-git.sh
+++ bash/65-git.sh
@@ -13,5 +13,6 @@ fi
 if [ -f $BASH_DIR/git-prompt.sh ]; then
     . git-prompt.sh
     export GIT_PS1_SHOWDIRTYSTATE=1
+    export GIT_PS1_SHOWUPSTREAM="auto,verbose,name"
     export PS1='\w$(__git_ps1 " (%s)")\$ '
 fi
