f2e13cacabe1f7f228eaacac48a5f7400946ea1a
Dave Jones <djones@minitab.com>
2017-02-10 22:04:46 -0500

    add setup, trash

---
+++ brew-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+
+brew install git --without-completions
+brew install zsh
+
+brew install tmux
+brew install ninja
+brew install ispell
+brew install node
+brew install clang-format
+brew install zsh
+brew install cmake
+brew install git
+brew install llvm
