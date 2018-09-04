a410545dfa2d132956df9315f79401c9b0c314e7
Dave Jones <dmj111@gmail.com>
2018-09-03 22:12:16 -0400

    Add code to auto writeout script and html

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,6 +2,23 @@
 # Created with:  jupyter notebook --generate-config
 
 # https://www.svds.com/jupyter-notebook-best-practices-for-data-science/
+# Reference: https://svds.com/jupyter-notebook-best-practices-for-data-science/
+# Can put this in a project directory instead of making it global
+import os
+from subprocess import check_call
+
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts"""
+    if model['type'] != 'notebook':
+        return # only do this for notebooks
+    d, fname = os.path.split(os_path)
+    check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
+    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
+
+
+c.FileContentsManager.post_save_hook = post_save
+
 # -----------------------------------------------------------------------------
 # Application(SingletonConfigurable) configuration
 # -----------------------------------------------------------------------------
