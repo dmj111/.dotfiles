37d18b99cab658020ef062486480f9f58701086a
Dave Jones <dmj111@gmail.com>
2020-05-19 20:16:39 -0400

    Move other tools into python package

--- dmj-tools/setup.py
+++ dmj-tools/setup.py
@@ -2,8 +2,15 @@ from setuptools import setup
 
 setup(
     name="dmj-tools",
-    version="0.2",
+    version="0.3",
     py_modules=["clean_git_gone"],
     install_requires=["Click", "gitpython"],
     entry_points={"console_scripts": ["git-clean-gone = clean_git_gone:cli"]},
+    scripts=[
+        "git-clean-trash",
+        "git-rsh",
+        "git-rsu",
+        "git-smg",
+        "git-trash",
+    ],
 )
