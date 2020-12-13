959e2181f7d138a62fdf41a60813f14719006c48
Dave Jones <dmj111@gmail.com>
2020-12-13 08:36:42 -0500

    Update theme code, add example of local config

--- home/.emacs.d/setup/03-theme.el
+++ home/.emacs.d/setup/03-theme.el
@@ -1,13 +1,14 @@
-(use-package zenburn-theme
-  :ensure t)
+;; (use-package zenburn-theme
+;;   :ensure t)
 
 
 ;; Set this var in local config
-(defvar my-default-theme 'zenburn
+(defvar my-default-theme nil
   "Default theme to use at startup.")
 
 ;; After init.el is loaded, set the theme.
 (eval-after-load 'init
   '(progn
-     (message "loading the theme...")
-     (load-theme my-default-theme t)))
+     (when 'my-default-theme
+       (message  "loading the theme...")
+       (load-theme my-default-theme t))))
---
+++ local-example/init-local.el
@@ -0,0 +1,14 @@
+(add-to-list 'exec-path "/usr/local/bin" t)
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq ispell-program-name "/usr/local/bin/aspell")
+
+;; (set-frame-font "source code pro-13")
+(set-frame-font "fira code-14")
+
+(add-to-list 'custom-theme-load-path "/Users/dave/.local-dotfiles/.emacs.d/emacs-color-theme-solarized")
+(setq my-default-theme 'solarized)
+
+(add-to-list 'exec-path "/usr/local/opt/llvm/bin/" t)
+
+(provide 'init-local)
+;;; init-local.el ends here
