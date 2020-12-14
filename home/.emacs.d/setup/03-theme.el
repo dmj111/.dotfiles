d5dcab4de6dc2a9736b9ca5c24354c477756a9fd
Dave Jones <dmj111@gmail.com>
2020-12-14 18:22:06 -0500

    Fix theme startup, stick with zenburn as default

--- home/.emacs.d/setup/03-theme.el
+++ home/.emacs.d/setup/03-theme.el
@@ -2,13 +2,17 @@
 ;;   :ensure t)
 
 
-;; Set this var in local config
-(defvar my-default-theme nil
+(use-package zenburn-theme
+  :ensure t
+  :defer t)
+
+;; Set this var in local config to override
+(defvar my-default-theme 'zenburn
   "Default theme to use at startup.")
 
 ;; After init.el is loaded, set the theme.
 (eval-after-load 'init
   '(progn
-     (when 'my-default-theme
-       (message  "loading the theme...")
+     (when my-default-theme
+       (message "loading the theme...")
        (load-theme my-default-theme t))))
