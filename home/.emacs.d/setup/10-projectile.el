7bc0d801709fbe1e8bcd975370fbb5ecb6502e54
Dave Jones <dmj111@gmail.com>
2020-12-23 16:29:39 -0500

    Show how to set search paths

--- home/.emacs.d/setup/10-projectile.el
+++ home/.emacs.d/setup/10-projectile.el
@@ -1,10 +1,11 @@
 (use-package projectile
   :ensure t
+  :after ivy
   :diminish projectile-mode
   :bind-keymap
   ("C-x p" . projectile-command-map)
   :config
   (setq projectile-completion-system 'ivy)
-  (setq projectile-indexing-method 'alien)
   (setq projectile-enable-caching t)
+  ;; (setq projectile-project-search-path '("~/code" "~/.dotfiles"))
   (projectile-mode t))
