55b0414bce0ee660d2f2e2b2ed322b4a9697897b
Dave Jones <dmj111@gmail.com>
2021-08-15 14:04:21 -0400

    Update notes for managing init files

--- .zsh/example-local.zsh
+++ .zsh/example-local.zsh
@@ -1,9 +1,11 @@
 
 # Setup for using git to control the init files
 
-alias config="$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias config="$(which git) --git-dir=$HOME/code/.dotfiles/ --work-tree=$HOME"
 # checkout a branch:
 # config checkout
+# alternatively, set GIT_DIR and GIT_WORK_TREE env variable in a shell to
+# work in the config directory
 
 # Create the local repo if needed
 # git init --bare $HOME/code/.local-dotfiles/
