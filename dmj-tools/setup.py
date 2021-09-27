ec91aeaaa1e5b7dd07ab0c366dd902b0b525c03c
Dave Jones <dmj111@gmail.com>
2021-09-27 14:19:16 -0400

    Fix bug:  we need stdout to get the branch name

--- dmj-tools/setup.py
+++ dmj-tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dmj-tools",
-    version="0.6",
+    version="0.7",
     py_modules=["clean_git_gone"],
     install_requires=["Click", "gitpython"],
     entry_points={"console_scripts": ["git-clean-gone = clean_git_gone:cli"]},
