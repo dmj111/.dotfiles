655397b1827ece7094a35e801edf3a040bb1506a
Dave Jones <dmj111@gmail.com>
2021-10-10 14:15:57 -0400

    Setup for homebrew

--- bin/brew-setup.sh
+++ bin/brew-setup.sh
@@ -3,20 +3,46 @@
 set -x
 
 # Shell
-brew install git --without-completions
 brew install tmux
+brew install tree
 brew install zsh
+brew install zsh-completions
+# brew install git --without-completions
 
 
 # C++
+brew install bazel
+brew install ccache
 brew install clang-format
 brew install cmake
+brew install eigen
 brew install llvm
 brew install ninja
 
-# js
-brew install node
 
 # Emacs
 brew cask install emacs
-brew install ispell
+brew install aspell
+
+
+
+# Personal
+brew install ledger
+
+# scala
+brew install sbt
+brew install scala
+
+# Js
+brew install node
+
+# Utils
+brew install graphviz
+brew install ghostscript
+
+
+# kubernetes
+brew install hyperkit
+brew install minikube
+brew install kubernetes-cli
+
