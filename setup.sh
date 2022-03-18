cfc079a3e7b2bc1b90234e2e9b093c1f50f6d46b
Dave Jones <dmj111@gmail.com>
2022-03-17 21:35:51 -0400

    Improve setup script to avoid stomping on files

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-
 # Consider:
 # zsh/local/locals.zsh
 #     fpath=(<brew_location>/share/zsh-completions $fpath)
@@ -14,21 +13,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd $DIR/home
 for f in $(ls -A); do
-    echo $f
-    rm -f ${HOME}/${f}
     src=${DIR}/home/${f}
     dst=${HOME}/${f}
-    ln -sf ${src} ${dst}
-    echo $src $dst
-    diff ${src} ${dst}
+    if [ "${src}" -ef "${dst}" ]; then
+        echo "   ${src} and ${dst}  are the same"
+    elif [ -e "${dst}" ]; then
+        echo "$dst exists and is different than $src, not modifying"
+    elif [ -d "${src}" ]; then
+        echo "adding symbolic link from ${src} to ${dst}"
+        ln -s "${src}" "${dst}"
+    else
+        echo "adding hard link from ${src} to ${dst}"
+        ln  "${src}" "${dst}"
+    fi
 done
-
-# for f in "${files[@]}"; do
-#     echo $f
-#     # rm -f ${HOME}/${f}
-#     src=${DIR}/${f}
-#     dst=${HOME}/${f}
-#     echo $src $dst
-#     ln -sf ${src} ${dst}
-#     diff ${src} ${dst}
-# done
