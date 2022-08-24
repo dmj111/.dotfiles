e5d96738bd14853e2f460cab4eeb12a69ea98cba
Dave Jones <dmj111@gmail.com>
2022-08-24 19:06:16 -0400

    Move org font settings away

--- home/.emacs.d/setup/10-org.el
+++ home/.emacs.d/setup/10-org.el
@@ -1,3 +1,68 @@
+(defvar *my-org-font-heading* nil
+  "Which font to use.  Can be set by the local init scripts.")
+
+
+(setq *my-org-font-heading*
+      (cond ((x-list-fonts "Avenir")         '(:font "Avenir"))
+            ((x-list-fonts "Charter")         '(:font "Charter"))
+            ((x-list-fonts "Palatino")         '(:font "Palatino"))
+            ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+            (t    '(:family "Sans Serif"))))
+
+(defvar *my-org-font-variable* nil
+  "Which font to use.  Can be set by the local init scripts.")
+
+
+(setq *my-org-font-variable*
+  (cond
+   ((x-list-fonts "Palatino")         '(:font "Palatino"))
+   ((x-list-fonts "Charter")         '(:font "Charter"))
+   (t    '(:family "Serif"))))
+
+
+(defvar *my-org-font-fixed* nil
+  "Which font to use.  Can be set by the local init scripts.")
+
+
+(setq *my-org-font-fixed*
+  (cond ((x-list-fonts "Fira Code")         '(:font "Fira Code"))
+        (t    '(:family "Monospace"))))
+
+
+(defun my-org-fonts-setup ()
+  (interactive)
+  (when window-system
+    (add-hook 'org-mode-hook 'variable-pitch-mode)
+
+    (let* ((heading-font *my-org-font-heading*)
+           (headline           `(:weight bold )))
+      (custom-theme-set-faces
+       'user
+       `(org-level-8 ((t (,@headline ,@heading-font))))
+       `(org-level-7 ((t (,@headline ,@heading-font))))
+       `(org-level-6 ((t (,@headline ,@heading-font))))
+       `(org-level-5 ((t (,@headline ,@heading-font))))
+       `(org-level-4 ((t (,@headline ,@heading-font :height 1.1))))
+       `(org-level-3 ((t (,@headline ,@heading-font :height 1.25))))
+       `(org-level-2 ((t (,@headline ,@heading-font :height 1.5))))
+       `(org-level-1 ((t (,@headline ,@heading-font :height 1.75))))
+       `(org-document-title ((t (,@headline ,@heading-font :height 2.0 :underline nil))))
+       `(variable-pitch ((t (:height 180 :weight thin ,@*my-org-font-variable*))))
+       `(fixed-pitch ((t (:height 160 ,@*my-org-font-fixed*))))
+       '(org-block ((t (:inherit fixed-pitch))))
+       '(org-code ((t (:inherit (shadow fixed-pitch)))))
+       ;; '(org-document-info ((t (:foreground "dark orange"))))
+       '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+       '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+       ;;   '(org-link ((t (:foreground "royal blue" :underline t))))
+       '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+       '(org-property-value ((t (:inherit fixed-pitch))) t)
+       '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+       ;;   '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+       '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+       '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))))))
+
+
 (use-package org
   :bind (([f6] . org-capture))
   :config
@@ -61,53 +126,8 @@ Will work on both org-mode and any mode that accepts plain html."
                           '(("^ *\\([-]\\) "
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-  (let* ((variable-tuple
-          (cond ((x-list-fonts "Avenir")         '(:font "Avenir"))
-                ((x-list-fonts "Charter")         '(:font "Charter"))
-                ((x-list-fonts "Palatino")         '(:font "Palatino"))
-                ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-                ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-                (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-         (base-font-color     (face-foreground 'default nil 'default))
-         (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-    (custom-theme-set-faces
-     'user
-     `(org-level-8 ((t (,@headline ,@variable-tuple))))
-     `(org-level-7 ((t (,@headline ,@variable-tuple))))
-     `(org-level-6 ((t (,@headline ,@variable-tuple))))
-     `(org-level-5 ((t (,@headline ,@variable-tuple))))
-     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
-
-  (custom-theme-set-faces
-   'user
-   '(variable-pitch ((t (:family "Charter" :height 180 :weight thin))))
-   '(fixed-pitch ((t ( :family "Fira Code" :height 160)))))
-
-  (add-hook 'org-mode-hook 'variable-pitch-mode)
-  (add-hook 'org-mode-hook 'visual-line-mode)
-  (custom-theme-set-faces
-   'user
-   '(org-block ((t (:inherit fixed-pitch))))
-   '(org-code ((t (:inherit (shadow fixed-pitch)))))
-   '(org-document-info ((t (:foreground "dark orange"))))
-   '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
-   '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
-   '(org-link ((t (:foreground "royal blue" :underline t))))
-   '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-   '(org-property-value ((t (:inherit fixed-pitch))) t)
-   '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-   '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
-   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
-   '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
-
-
-  (setq org-hide-emphasis-markers t))
-
+  (setq org-hide-emphasis-markers t)
+  (my-org-fonts-setup))
 
 
 
