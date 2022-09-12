0edff16883b6e510471df53b02bd149f8a75d555
Dave Jones <dmj111@gmail.com>
2022-09-12 14:24:41 -0400

    cleanup flycheck

--- home/.emacs.d/setup/09-flycheck.el
+++ home/.emacs.d/setup/09-flycheck.el
@@ -4,11 +4,12 @@
   ;; Force flycheck to always use c++11 support. We use
   ;; the clang language backend so this is set to clang
 
-    ;; Turn flycheck on everywhere
-  ;; (global-flycheck-mode)
+  ;; Turn flycheck on everywhere
+  (global-flycheck-mode 1)
   (add-hook 'c++-mode-hook
             (lambda () (setq flycheck-clang-language-standard "c++14")))
 
   ;; Requires pylint and flake8 to be installed.
   (flycheck-add-next-checker `python-pylint '(warning . python-flake8))
+
   (setq  flycheck-python-flake8-executable "flake8"))
