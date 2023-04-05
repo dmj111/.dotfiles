8fc3f5d41d7e1a7f5ce90e452fcee86433afff9c
Dave Jones <dmj111@gmail.com>
2023-04-04 22:35:28 -0400

    Switch back to symlinks

--- setup.sh
+++ setup.sh
@@ -10,20 +10,39 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #     fpath=(<brew_location>/share/zsh-completions $fpath)
 #     path=(<brew_location>/bin $path)
 
+#!/usr/bin/env bash
+
+safeln() {
+    local src dst
+    src=$1
+    shift
+    dst=$1
+    shift
+    echo "${src} -> ${dst}"
+
+    # Full paths work, or relative paths to the dst if you are in the dst's directory
+    if [ ! -e "${src}" ]; then
+        echo "src does not exist"
+    elif [ "${src}" -ef "${dst}" -a -h "${dst}" ]; then
+        echo "they are the same"
+    else
+        if [ -e "${dst}" -o -h "${dst}" ]; then
+            local f
+            # Move original out of the way
+            f=$(date "+${dst}.%Y%m%d_%H%M%S")
+            echo "Copying ${dst} to ${f}"
+            mv -n ${dst} ${f}
+        fi
+        ln -s ${src} ${dst}
+    fi
+}
+
 
 pushd $DIR/home
 for f in $(ls -A); do
     src=${DIR}/home/${f}
     dst=${HOME}/${f}
-    if [ "${src}" -ef "${dst}" ]; then
-        echo "   ${src} and ${dst}  are the same"
-    elif [ -e "${dst}" ]; then
-        echo "$dst exists and is different than $src, not modifying"
-    elif [ -d "${src}" ]; then
-        echo "adding symbolic link from ${src} to ${dst}"
-        ln -s "${src}" "${dst}"
-    else
-        echo "adding hard link from ${src} to ${dst}"
-        ln  "${src}" "${dst}"
-    fi
+    safeln "${src}" "${dst}"
 done
+
+unset -f safeln
