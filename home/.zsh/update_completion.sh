9202510a5e9a50c62d28d5defcbded8fa4af7e24
Dave Jones <dmj111@gmail.com>
2023-04-08 23:11:44 -0400

    Remove extra mv from update script

--- home/.zsh/update_completion.sh
+++ home/.zsh/update_completion.sh
@@ -7,5 +7,4 @@
 # Download the scripts
 curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
-
 rm -f ~/.zcompdump
