fa90da4e2971ab65052bfb484363df78bbd1e917
Dave Jones <dmj111@gmail.com>
2023-05-07 13:29:16 -0400

    Move git scripts out of python package
    While fun, using a python package for these was unnecessary.
    
    The git-clean-gone script still needs python though, so leaving it
    makes sense,

--- dmj-tools/setup.py
+++ dmj-tools/setup.py
@@ -2,15 +2,10 @@ from setuptools import setup
 
 setup(
     name="dmj-tools",
-    version="0.7",
+    version="0.8",
     py_modules=["clean_git_gone"],
     install_requires=["Click", "gitpython"],
     entry_points={"console_scripts": ["git-clean-gone = clean_git_gone:cli"]},
     scripts=[
-        "git-clean-stash",
-        "git-rsh",
-        "git-rsu",
-        "git-smg",
-        "git-trash",
     ],
 )
