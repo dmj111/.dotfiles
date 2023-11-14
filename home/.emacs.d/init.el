614734e7aa89a2310fb9650f0474485a12f43d7f
Dave Jones <dmj111@gmail.com>
2023-11-14 18:28:17 -0500

    Update capture settings

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -628,20 +628,20 @@ Will work on both org-mode and any mode that accepts plain html."
   :bind (([f6] . org-capture)
          ("\C-cc" . org-capture))
   :custom
-  (org-capture-templates   '(("r" "Reference" entry (file "reference.org")
-     "* %? %^g" :prepend t)
-    ;; ("t" "Todo Inbox" entry (file+headline "" "Todos")
-    ;;  "* TODO %?\n  Added %u\n  %i" :prepend t)
-    ("b" "Bookmark" entry (file "bookmarks.org")
-     "* %?\n %I")
-    ;; http://members.optusnet.com.au/~charles57/GTD/datetree.html
-    ("n" "Notes inbox" entry (file+headline "log.org" "Inbox")
-     "* %^{Description} %^g %?
-Added: %U")
-    ("t" "make a task" entry (file+olp+datetree "log.org")
-     "* TODO %^{Description} %^g
- %?
-Added: %U"))))
+  (org-capture-templates
+   '(("r" "Reference" entry (file "reference.org")
+      "* %? %^g" :prepend t)
+     ;; ("t" "Todo Inbox" entry (file+headline "" "Todos")
+     ;;  "* TODO %?\n  Added %u\n  %i" :prepend t)
+     ("b" "Bookmark" entry (file "bookmarks.org")
+      "* [[%^{link}][%^{description}]] %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
+     ;; ("b" "Bookmark" entry (file "bookmarks.org")
+     ;;  "* %?\n %x")
+     ;; http://members.optusnet.com.au/~charles57/GTD/datetree.html
+     ("n" "Notes inbox" entry (file+headline "log.org" "Inbox")
+      "* %^{Description} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?\n" :empty-lines 1)
+     ("t" "make a task" entry (file+olp+datetree "log.org")
+      "* TODO %^{Description} %^g\n\n:PROPERTIES:\n:CREATED: %U\n:END:\n"))))
 
 (use-package org-refile
   :defer t
